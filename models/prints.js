0fbbd5ce9908ec77247d04d9a083fcf4e1fbcae5
ddesrochers99 <desrochersdale@gmail.com>
2023-07-17 13:53:17 -0700

    Adds all ejs pages and correctly links to pages

--- models/prints.js
+++ models/prints.js
@@ -52,5 +52,4 @@ const printSchema = new Schema(
   }
 );
 
-// Compile the schema into a model and export it
 module.exports = mongoose.model("Print", printSchema);
--- routes/print.js
+++ routes/print.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/prints/new", (req, res) => {
-  res.render("prints/new");
+router.get("/new", function (req, res) {
+  res.render("prints/new", { title: "" }); 
 });
 
+router.get("/all", function (req, res) {
+  res.render("prints/allprints", { title: "" }); 
+});
 module.exports = router;
