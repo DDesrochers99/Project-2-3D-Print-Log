30a1294dfda0b35d5ab132db9f4aa606ea9ae35d
ddesrochers99 <desrochersdale@gmail.com>
2023-07-20 13:51:09 -0700

    Adds aprints w/verification file

--- routes/print.js
+++ routes/print.js
@@ -4,13 +4,13 @@ const router = express.Router();
 const printController = require("../controllers/print");
 const ensureLoggedIn = require("../config/ensureLoggedIn");
 
-router.get("/aprints", printController.getAllPrints);
+router.get("/aprints", ensureLoggedIn, printController.getAllPrints);
 
 router.get("/new", ensureLoggedIn, printController.createPrint);
 
 router.get("/prints", ensureLoggedIn, printController.getAllPrints);
 
-router.get("/:id", printController.getPrintDetails);
+router.get("/:id", ensureLoggedIn, printController.getPrintDetails);
 
 router.post("/", ensureLoggedIn, printController.create);
 
